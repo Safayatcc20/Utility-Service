7cc60d0bef86ca38261b210a5e9f208610bc6ac5
Safayatcc20 <mdsafayatcc20@gmail.com>
2023-10-30 19:08:52 +0600

    Developed By Safayat (30-10-2023)

---
+++ src/org/xemocode/demo/login.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package org.xemocode.demo;
+import javax.swing.JFrame;
+import org.xemocode.demo.Log_In;
+/**
+ *
+ * @author Saif
+ */
+public class login {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame login = new Log_In();
+        login.setVisible(true);
+    }
+    
+}
